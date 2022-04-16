3330bef820e3ed5f656b1051cdb3932ffe2ecb9b
FatCat68 <100338252+FatCaToops@users.noreply.github.com>
2022-04-15 19:41:52 -0700

    fix java file error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,13 +10,19 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
+        
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(openBracket > 0){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            else{
+                break;
+            }
             currentIndex = closeParen + 1;
         }
 
